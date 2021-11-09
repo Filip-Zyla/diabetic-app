d556e38668ea60e4126065274e812e059fe2ce72
Filip Żyła <fzyla99@gmail.com>
2021-11-09 12:50:40 +0100

    App booting up, h2db connected

---
+++ src/main/java/com/filipzyla/diabeticapp/DiabeticAppApplication.java
@@ -0,0 +1,13 @@
+package com.filipzyla.diabeticapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeticAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiabeticAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filipzyla/diabeticapp/DiabeticAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipzyla.diabeticapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeticAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
