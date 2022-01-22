b087abaca99c93e1dcc41c83a5851f32deec7cd3
Filip Żyła <fzyla99@gmail.com>
2022-01-22 21:15:27 +0100

    Password encrypted in properties

--- src/main/java/com/filipzyla/diabeticapp/DiabeticAppApplication.java
+++ src/main/java/com/filipzyla/diabeticapp/DiabeticAppApplication.java
@@ -1,11 +1,13 @@
 package com.filipzyla.diabeticapp;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
+@EnableEncryptableProperties
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class DiabeticAppApplication {
 
