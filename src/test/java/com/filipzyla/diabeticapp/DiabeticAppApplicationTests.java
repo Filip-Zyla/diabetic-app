e7c45128d1f22e2718da55964239aa5d313b3c7f
Filip Żyła <fzyla99@gmail.com>
2022-01-24 21:30:08 +0100

    Changes from vaadin template

--- src/test/java/com/filipzyla/diabeticapp/DiabeticAppApplicationTests.java
+++ src/test/java/com/filipzyla/diabeticapp/DiabeticAppApplicationTests.java
@@ -1,13 +1,7 @@
 package com.filipzyla.diabeticapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DiabeticAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
